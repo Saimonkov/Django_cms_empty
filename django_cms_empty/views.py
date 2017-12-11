0d9cc063939d1ceff56fa12687fdad6cf54f5602
Sergey <saimon.kovalchuk@yandex.ru>
2017-12-11 16:55:52 +0600

    commit

---
+++ django_cms_empty/views.py
@@ -0,0 +1,13 @@
+#from django.shortcuts import render
+#import urllib
+# import datetime
+#from xml.etree import ElementTree as ET
+
+
+# from djangocms_forms.forms import FormBuilder
+
+
+# from djangocms_forms.views import FormSubmission
+#from djangocms_forms.models import FormSubmission, FormDefinition
+
+
